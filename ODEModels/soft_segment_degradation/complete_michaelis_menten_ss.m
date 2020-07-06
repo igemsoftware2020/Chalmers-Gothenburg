2ca1150c2c55b148a9926c647e21e947a6e6c21c
leticiacastillon <60732799+leticiacastillon@users.noreply.github.com>
2020-07-06 16:55:21 +0200

    Script without EG flux towards CCM

--- ODEModels/soft_segment_degradation/complete_michaelis_menten_ss.m
+++ ODEModels/soft_segment_degradation/complete_michaelis_menten_ss.m
@@ -3,24 +3,18 @@ function y=enzym(z,c);
 %value for enzyme total concentration
 enzyme_concentration=0.2; %random value 
 
-%vmax oxidorreductase (degradation of ethylene glycol)
-vmaxOxidorreductase = 5.7;
-
 %km values for enzymes
 kmPet=4.6;
 kmMhet=3.9;
 kmPegdh=2.0; %chose an average value within a range, see excel file
-kmOxidorreductase = 0.007; %degradation of ethylene glycol
 
 %kcat values for the enzymes: pet, mhet,peg-dh,puea,deaminase [1/s]
 kcatPet=5.9;
 kcatMhet=26.8;
 kcatPegdh=10; %check for correct value, this one is random
-kcatOxidorreductase = ((kmOxidorreductase + c(3))/(enzyme_concentration*c(3)))*vmaxOxidorreductase; %calculated from km and vmax
-
 
 y=[(-kcatPet.*c(1).*enzyme_concentration)./(kmPet+c(1))
     (kcatPet.*c(1).*enzyme_concentration)./(kmPet+c(1))- (kcatMhet.*c(2).*enzyme_concentration)./(kmMhet+c(2))
-    (kcatMhet.*c(2).*enzyme_concentration)./(kmMhet+c(2))+ (kcatPegdh.*c(4).*enzyme_concentration)./(kmPegdh+c(4)) - (kcatOxidorreductase.*c(3).*enzyme_concentration)./(kmOxidorreductase+c(3))
+    (kcatMhet.*c(2).*enzyme_concentration)./(kmMhet+c(2))+ (kcatPegdh.*c(4).*enzyme_concentration)./(kmPegdh+c(4)) 
    -(kcatPegdh.*c(4).*enzyme_concentration)./(kmPegdh+c(4))
    ];
